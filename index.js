1a3b05d9eec89fe3a375f44bd4672e38ecd5a71f
ivuckovi@sfu.ca <ivuckovi@sfu.ca>
2020-06-18 12:24:53 -0700

    DATATABLE 3000

--- index.js
+++ index.js
@@ -1,129 +1,168 @@
-import React, { useState } from "react"
-import PropTypes from "prop-types"
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { FaSort, FaSortUp, FaSortDown } from 'react-icons/fa';
-import "./styles.css"
-function DataTable({ tableStyle, data, columns }) {
-    const [sortOrder, setSortOrder] = useState([])
-    const [sortDirection, setSortDirection] = useState([])
-    const [rows, setRows] = useState([])
-    if (rows.length === 0) {
-        console.log("###")
-        setRows([...data])
-    }
-    function onSort(event) {
-        let tempRows = [...data]
-        let tempOrder = sortOrder
-        let tempDirection = sortDirection
-        if (event.currentTarget.id) {
-            let index = tempOrder.indexOf(event.currentTarget.id)
-            if (index === -1) {
-                tempOrder.push(event.currentTarget.id)
-                tempDirection.push(1)
+import './styles.css';
+
+function DataTable({
+  tableStyle,
+  headerStyle,
+  customClassName,
+  data,
+  columns,
+}) {
+  const [sortOrder, setSortOrder] = useState([]);
+  const [sortDirection, setSortDirection] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  if (rows.length === 0) {
+    setRows([...data]);
+  }
+
+  function onSort(event) {
+    const tempRows = [...data];
+    const tempOrder = sortOrder;
+    const tempDirection = sortDirection;
+    if (event.currentTarget.id) {
+      const index = tempOrder.indexOf(event.currentTarget.id);
+      if (index === -1) {
+        tempOrder.push(event.currentTarget.id);
+        tempDirection.push(1);
+      } else if (tempDirection[index] === 1) {
+        tempOrder.splice(index, 1);
+        tempOrder.push(event.currentTarget.id);
+        tempDirection.splice(index, 1);
+        tempDirection.push(-1);
+      } else if (tempDirection[index] === -1) {
+        tempOrder.splice(index, 1);
+        tempDirection.splice(index, 1);
+      }
+      tempRows.sort((a, b) => {
+        let temp;
+        for (let i = 0; i < tempOrder.length; i += 1) {
+          const sortKey = tempOrder[i];
+          if (typeof a[sortKey] === 'number') {
+            temp =
+              a[sortKey]
+                .toString()
+                .localeCompare(b[sortKey].toString(), 'en-US', {
+                  numeric: 'true',
+                }) * tempDirection[i];
+            if (temp !== 0) {
+              return temp;
             }
-            else if (tempDirection[index] === 1) {
-                tempOrder.splice(index, 1)
-                tempOrder.push(event.currentTarget.id)
-                tempDirection.splice(index, 1)
-                tempDirection.push(-1)
+          } else {
+            temp = a[sortKey].localeCompare(b[sortKey]) * tempDirection[i];
+            if (temp !== 0) {
+              return temp;
             }
-            else if (tempDirection[index] === -1) {
-                tempOrder.splice(index, 1)
-                tempDirection.splice(index, 1)
-            }
-            tempRows.sort(
-                (a, b) => {
-                    let temp;
-                    for (let i = 0; i < tempOrder.length; i++) {
-                        let sortKey = tempOrder[i]
-                        if (typeof a[sortKey] === "number") {
-                            temp = a[sortKey]
-                                .toString()
-                                .localeCompare(b[sortKey].toString(), "en-US", {
-                                    numeric: "true",
-                                }) * tempDirection[i]
-                            if (temp !== 0) {
-                                return temp
-                            }
-                        } else {
-                            temp = a[sortKey].localeCompare(b[sortKey]) * tempDirection[i]
-                            if (temp !== 0) {
-                                return temp
-                            }
-                        }
-                    }
-                    return 0
-                }
-            )
-            console.log(tempRows)
-            setRows([...tempRows])
-            setSortOrder([...tempOrder])
-            setSortDirection([...tempDirection])
+          }
         }
+        return 0;
+      });
+
+      setRows([...tempRows]);
+      setSortOrder([...tempOrder]);
+      setSortDirection([...tempDirection]);
     }
-    const tableStyleDefault = {
-        backgroundColor: "rgb(255,255,204)",
-        border: "rgb(0,0,0) solid 2px"
-    }
-    console.log(rows)
-    return (
-        <table cellSpacing="0" cellPadding="5px" width="100%" border="0" style={tableStyle || tableStyleDefault}>
-            <thead>
-                <tr>
-                    {columns.map((column) => (
-                        <th
-                            key={column.header}
-                            id={column.dataID}
-                            className={column.className || "search-result-heading"}
-                            width={column.width}
-                            style={{ textAlign: column.align || "left" }}
-                            onClick={(column.sortable) ? onSort : () => { }}
-                        >
-                            <div title={column.helper || ""}
-                                style={{ color: "white", ...column.headerStyle } || { color: "white" }}>
-                                {column.header}{(column.sortable) &&
-                                    (sortOrder.indexOf(column.dataID) === -1 ?
-                                        <FaSort />
-                                        : (sortDirection[sortOrder.indexOf(column.dataID)] === 1 ?
-                                            <FaSortDown />
-                                            : <FaSortUp />))}
-                            </div>
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {rows.map((dataItem, index) => (
-                    <tr key={index}>
-                        {columns.map(column => (
-                            <td key={column.header} style={{ textAlign: column.align || "left" }}>
-                                <div style={{ color: "black", ...column.cellStyle } || { color: "black" }}>
-                                    <span className={(column.isLink) ? "tableLink" : ""} onClick={() => { column.clickCallback(dataItem) }}>{((column.dataID) ? dataItem[column.dataID] : column.customCell)}</span>
-                                </div>
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        </table >
-    )
+  }
+
+  return (
+    <table
+      cellSpacing="0"
+      cellPadding="5px"
+      width="100%"
+      border="0"
+      style={tableStyle}
+      id="datatable"
+    >
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th
+              key={column.header}
+              id={column.dataID}
+              className={customClassName}
+              width={column.width}
+              style={{ textAlign: column.align || 'left' }}
+              onClick={column.sortable ? onSort : () => {}}
+            >
+              <div title={column.helper || ''} style={{ ...headerStyle }}>
+                {column.header}
+                {column.sortable &&
+                  (sortOrder.indexOf(column.dataID) === -1 ? (
+                    <FaSort />
+                  ) : sortDirection[sortOrder.indexOf(column.dataID)] === 1 ? (
+                    <FaSortDown />
+                  ) : (
+                    <FaSortUp />
+                  ))}
+              </div>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((dataItem, index) => (
+          <tr key={index}>
+            {columns.map((column) => (
+              <td
+                key={column.header}
+                style={{ textAlign: column.align || 'left' }}
+              >
+                <div
+                  style={
+                    { color: 'black', ...column.columnStyle } || {
+                      color: 'black',
+                    }
+                  }
+                >
+                  <span
+                    className={column.isLink ? 'tableLink' : ''}
+                    onClick={() => {
+                      column.clickCallback(dataItem);
+                    }}
+                  >
+                    {column.dataID
+                      ? dataItem[column.dataID]
+                      : column.customCell}
+                  </span>
+                </div>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
+
 DataTable.propTypes = {
-    tableStyle: PropTypes.object,
-    data: PropTypes.array.isRequired,
-    columns: PropTypes.arrayOf(PropTypes.shape({
-        header: PropTypes.string.isRequired,
-        dataID: PropTypes.string,
-        width: PropTypes.string,
-        align: PropTypes.string,
-        headerStyle: PropTypes.object,
-        cellStyle: PropTypes.object,
-        sortable: PropTypes.bool,
-        customCell: PropTypes.object,
-        customClassName: PropTypes.string,
-        helper: PropTypes.string,
-        isLink: PropTypes.bool,
-        clickCallback: PropTypes.function,
-    })).isRequired
-}
+  tableStyle: PropTypes.shape(PropTypes.object),
+  headerStyle: PropTypes.shape(PropTypes.object),
+  customClassName: PropTypes.string,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      header: PropTypes.string.isRequired,
+      dataID: PropTypes.string,
+      width: PropTypes.string,
+      align: PropTypes.string,
+      sortable: PropTypes.bool,
+      customCell: PropTypes.object,
+      helper: PropTypes.string,
+      isLink: PropTypes.bool,
+      clickCallback: PropTypes.function,
+    })
+  ).isRequired,
+};
+
+DataTable.defaultProps = {
+  tableStyle: {
+    backgroundColor: 'rgb(255,255,204)',
+    border: 'rgb(0,0,0) solid 2px',
+  },
+  headerStyle: {},
+  customClassName: '',
+};
+
 export default DataTable;
--- tests/index.js
+++ tests/index.js
@@ -1,152 +1,161 @@
-import React from "react"
-import DataTable from "../"
+import React from 'react';
+import DataTable from '..';
 
 function Test() {
-    function handleTestClick(state) {
-        console.log(state)
-    }
-    const columns = [
-        {
-            header: 'Copy',
-            sortable: false,
-            customCell: <input type="button" value="test" />,
-            clickCallback: (state) => { console.log("this is a button"); console.log(state) },
-            headerStyle: {
-                color: "purple",
-            },
-            cellStyle: {
-                color: "red"
-            },
-            width: '70px',
-            helper: 'TEST',
-        },
-        {
-            header: 'SO#',
-            dataID: 'SO_ID',
-            clickCallback: (state) => { handleTestClick(state) },
-            isLink: true,
-            sortable: true,
-            width: '80px',
-        },
-        {
-            header: 'Customer ID',
-            dataID: 'CUSTOMER_ID',
-            sortable: true,
-            width: '80px',
-        },
-        {
-            header: 'Short Name',
-            dataID: 'CUSTOMER_SHORT_NAME',
-            sortable: true,
-            width: '100px',
-        },
-        {
-            header: 'Customer Name',
-            dataID: 'CUSTOMER_NAME',
-            align: 'left',
-            sortable: true,
-            width: '300px',
-        },
-        {
-            header: 'Order Type',
-            dataID: 'ORDER_TYPE_DESC',
-            sortable: true,
-        },
-        {
-            header: 'Order Status',
-            dataID: 'ORDER_STATUS_DESC',
-            sortable: true,
-        },
-        {
-            header: 'Created By',
-            dataID: 'EMAIL_ADDRESS',
-            sortable: true,
-            width: '250px',
-        },
-        {
-            header: 'Customer Service Rep',
-            dataID: 'CSR_CONTACT',
-            sortable: true,
-        },
-        {
-            header: 'Sales Rep Location',
-            dataID: 'SALES_REP_LOCATION',
-            sortable: true,
-        },
-        {
-            header: 'Care Received Date',
-            dataID: 'REQUEST_SUBMIT_DATE_TIME',
-            sortable: true,
-            width: '170px',
-        },
-    ];
-    const data = [
-        {
-            SO_ID: "123",
+  function handleTestClick(state) {
+    console.log(state);
+  }
+  const columns = [
+    {
+      header: 'Copy',
+      sortable: false,
+      customCell: <input type="button" value="test" />,
+      clickCallback: (state) => {
+        console.log('this is a button');
+        console.log(state);
+      },
+      headerStyle: {
+        color: 'green',
+      },
+      cellStyle: {
+        color: 'red',
+      },
+      width: '70px',
+      helper: 'TEST',
+    },
+    {
+      header: 'SO#',
+      dataID: 'SO_ID',
+      clickCallback: (state) => {
+        handleTestClick(state);
+      },
+      isLink: true,
+      sortable: true,
+      width: '80px',
+    },
+    {
+      header: 'Customer ID',
+      dataID: 'CUSTOMER_ID',
+      sortable: true,
+      width: '80px',
+    },
+    {
+      header: 'Short Name',
+      dataID: 'CUSTOMER_SHORT_NAME',
+      sortable: true,
+      width: '100px',
+    },
+    {
+      header: 'Customer Name',
+      dataID: 'CUSTOMER_NAME',
+      align: 'left',
+      sortable: true,
+      width: '300px',
+    },
+    {
+      header: 'Order Type',
+      dataID: 'ORDER_TYPE_DESC',
+      sortable: true,
+    },
+    {
+      header: 'Order Status',
+      dataID: 'ORDER_STATUS_DESC',
+      sortable: true,
+    },
+    {
+      header: 'Created By',
+      dataID: 'EMAIL_ADDRESS',
+      sortable: true,
+      width: '250px',
+    },
+    {
+      header: 'Customer Service Rep',
+      dataID: 'CSR_CONTACT',
+      sortable: true,
+    },
+    {
+      header: 'Sales Rep Location',
+      dataID: 'SALES_REP_LOCATION',
+      sortable: true,
+    },
+    {
+      header: 'Care Received Date',
+      dataID: 'REQUEST_SUBMIT_DATE_TIME',
+      sortable: true,
+      width: '170px',
+    },
+  ];
+  const data = [
+    {
+      SO_ID: '123',
 
-            CUSTOMER_ID: "123",
+      CUSTOMER_ID: '123',
 
-            CUSTOMER_SHORT_NAME: "123",
+      CUSTOMER_SHORT_NAME: '123',
 
-            CUSTOMER_NAME: "123",
+      CUSTOMER_NAME: '123',
 
-            ORDER_TYPE_DESC: "123",
+      ORDER_TYPE_DESC: '123',
 
-            ORDER_STATUS_DESC: "123",
+      ORDER_STATUS_DESC: '123',
 
-            EMAIL_ADDRESS: "123",
+      EMAIL_ADDRESS: '123',
 
-            CSR_CONTACT: "123",
+      CSR_CONTACT: '123',
 
-            SALES_REP_LOCATION: "123",
+      SALES_REP_LOCATION: '123',
 
-            REQUEST_SUBMIT_DATE_TIME: "123",
-        },
-        {
-            SO_ID: "235",
+      REQUEST_SUBMIT_DATE_TIME: '123',
+    },
+    {
+      SO_ID: '235',
 
-            CUSTOMER_ID: "456",
+      CUSTOMER_ID: '456',
 
-            CUSTOMER_SHORT_NAME: "123",
+      CUSTOMER_SHORT_NAME: '123',
 
-            CUSTOMER_NAME: "123",
+      CUSTOMER_NAME: '123',
 
-            ORDER_TYPE_DESC: "123",
+      ORDER_TYPE_DESC: '123',
 
-            ORDER_STATUS_DESC: "123",
+      ORDER_STATUS_DESC: '123',
 
-            EMAIL_ADDRESS: "123",
+      EMAIL_ADDRESS: '123',
 
-            CSR_CONTACT: "123",
+      CSR_CONTACT: '123',
 
-            SALES_REP_LOCATION: "123",
+      SALES_REP_LOCATION: '123',
 
-            REQUEST_SUBMIT_DATE_TIME: "123",
-        },
-        {
-            SO_ID: "235",
+      REQUEST_SUBMIT_DATE_TIME: '123',
+    },
+    {
+      SO_ID: '235',
 
-            CUSTOMER_ID: "123",
+      CUSTOMER_ID: '123',
 
-            CUSTOMER_SHORT_NAME: "123",
+      CUSTOMER_SHORT_NAME: '123',
 
-            CUSTOMER_NAME: "123",
+      CUSTOMER_NAME: '123',
 
-            ORDER_TYPE_DESC: "123",
+      ORDER_TYPE_DESC: '123',
 
-            ORDER_STATUS_DESC: "123",
+      ORDER_STATUS_DESC: '123',
 
-            EMAIL_ADDRESS: "123",
+      EMAIL_ADDRESS: '123',
 
-            CSR_CONTACT: "123",
+      CSR_CONTACT: '123',
 
-            SALES_REP_LOCATION: "123",
+      SALES_REP_LOCATION: '123',
 
-            REQUEST_SUBMIT_DATE_TIME: "123",
-        },
-    ]
-    return (
-        <DataTable columns={columns} data={data} />
-    )
+      REQUEST_SUBMIT_DATE_TIME: '123',
+    },
+  ];
+  return (
+    <DataTable
+      customClassName="search-result-heading"
+      columns={columns}
+      data={data}
+    />
+  );
 }
 export default Test;
